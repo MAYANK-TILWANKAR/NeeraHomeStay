1ffe846d16cf017162574b3b56359595ce8c2d26
NeeraHomeStay <adisharma151ays@gmail.com>
2024-10-18 19:49:02 +0530

    oki

--- pages/api/getDetails.ts
+++ pages/api/getDetails.ts
@@ -1,18 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import connectToDatabase from "../../lib/mongodb";
 import User from "../../models/User";
 
-export async function GET() {
-  try {
-    await connectToDatabase();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    try {
+      await connectToDatabase();
 
-    const users = await User.find({}).sort({ dateFrom: -1 });
+      const users = await User.find({}).sort({ dateFrom: -1 });
 
-    return NextResponse.json(users, { status: 200 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Error fetching user details", error },
-      { status: 500 }
-    );
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: "Error fetching user details", error });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
